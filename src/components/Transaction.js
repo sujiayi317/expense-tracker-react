0d94dc5715083ca594d6feb29f6a52163b891835
Jiayi Su <sjy.su@mail.utoronto.ca>
2021-06-12 18:24:22 -0400

    Finish Transaction.js

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 export const Transaction = ({ transaction }) => {
+    const sign = transaction.amount < 0 ? '-' : '+';
+    
     return (
-        <li class="minus">
-          {transaction.text} <span>-$400</span><button class="delete-btn">x</button>
+        <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
+          {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span><button class="delete-btn">x</button>
         </li>
     )
 }
